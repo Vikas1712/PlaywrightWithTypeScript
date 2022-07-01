d867d602b2d8ee39b3a4744eb3128671cc73abf9
Vikas Yadav <dnsvikas.wins@gmail.com>
2022-07-01 20:33:01 +0200

    first test case added

---
+++ tests/launchBrowser.test.ts
@@ -0,0 +1,16 @@
+import { test, expect, type Page } from '@playwright/test';
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('https://letcode.in/');
+}); 
+
+
+test('should able to login into application', async ({ page }) => {
+    // Create one todo item.
+    //await page.goto('https://letcode.in/');
+    await page.click("text=Log in");
+    await page.fill("input[name='email']",'dnsvikas.wins@gmail.com');
+    await page.fill("input[name='password']",'Password@12345');
+    await page.click('button:text("LOGIN")');
+    await page.click('"Sign out"');
+  });
